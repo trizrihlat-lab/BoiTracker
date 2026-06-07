2c32c79c34eeb73bd30e0062e48de163182b9c91
trizrihlat-lab <trizrihlat@gmail.com>
2026-06-07 17:29:20 +0600

    Create books.js

---
+++ books.js
@@ -0,0 +1,19 @@
+const books = [
+  {
+    id:1,
+    title:"Atomic Habits",
+    author:"James Clear",
+    price:"320",
+    image:"https://covers.openlibrary.org/b/isbn/9780735211292-L.jpg",
+    affiliate:"#"
+  },
+
+  {
+    id:2,
+    title:"Rich Dad Poor Dad",
+    author:"Robert Kiyosaki",
+    price:"280",
+    image:"https://covers.openlibrary.org/b/isbn/9781612680194-L.jpg",
+    affiliate:"#"
+  }
+];
